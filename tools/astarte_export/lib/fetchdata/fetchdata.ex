c0be348b1c8fd6d172e59ccf0ba96bc21e71458a
Harika <abhilash.ramadugu52@gmail.com>
2020-01-03 07:49:21 +0530

    Added changes to support multiple data_types

--- tools/astarte_export/lib/fetchdata/fetchdata.ex
+++ tools/astarte_export/lib/fetchdata/fetchdata.ex
@@ -1,14 +1,11 @@
 defmodule Astarte.Export.FetchData do
-  alias Astarte.Core.CQLUtils
   alias Astarte.Core.Device
   alias Astarte.Core.Mapping
   alias Astarte.Core.Mapping.EndpointsAutomaton
   alias Astarte.DataAccess.Database
   alias Astarte.DataAccess.Interface
   alias Astarte.DataAccess.Mappings
-  alias Astarte.Import
-  alias CQEx.Query
-  alias CQEx.Result
+  alias Astarte.Export.FetchData.Queries
   require Logger
 
   defmodule State do
@@ -32,74 +29,70 @@ defmodule Astarte.Export.FetchData do
     ]
   end
 
-  @spec get_connection(String.t()) :: {:ok, identifier()}
+  @spec process_device_data(identifier(), String.t(), list()) :: struct()
 
-  def get_connection(realm) do
-    {:ok, conn} = Database.connect(realm)
-    {:ok, conn}
-  end
-
-  @spec get_devices(identifier()) :: list()
-
-  def get_devices(conn) do
-    statement = "SELECT * from devices;"
-
-    CQEx.Query.call!(conn, statement)
-    |> Enum.to_list()
-  end
-
-  @spec process_device_data(identifier(), list()) :: struct()
-
-  def process_device_data(conn, device_data) do
-    device_id = device_data[:device_id] |> Device.encode_device_id()
+  def process_device_data(conn, realm, device_data) do
+    #    device_id =
+    #      device_data[:device_id]
+    #      |> Device.encode_device_id()
+    device_id = 0
     revision = device_data[:protocol_revision]
-    pending_empty_cache = device_data[:pending_empty_cache] |> to_string |> String.downcase()
+
+    pending_empty_cache =
+      device_data[:pending_empty_cache]
+      |> to_string
+      |> String.downcase()
+
     secret_bcrypt_hash = device_data[:credentials_secret]
 
     first_registration =
       device_data[:first_registration]
-      |> DateTime.from_unix!(:microsecond)
-      |> DateTime.to_string()
+      |> DateTime.from_unix!(:millisecond)
+      |> DateTime.to_iso8601()
 
-    inhibit_request = device_data[:inhibit_credentials_request] |> to_string |> String.downcase()
+    inhibit_request =
+      device_data[:inhibit_credentials_request]
+      |> to_string
+      |> String.downcase()
 
     cert_serial = device_data[:cert_serial]
+
     cert_aki = device_data[:cert_aki]
 
     first_credentials_request =
       device_data[:first_credentials_request]
       |> DateTime.from_unix!(:millisecond)
-      |> DateTime.to_string()
+      |> DateTime.to_iso8601()
 
     last_credentials_request_ip =
       device_data[:last_credentials_request_ip]
       |> :inet_parse.ntoa()
-      |> Kernel.to_string()
+      |> to_string()
 
     total_received_msgs =
       device_data[:total_received_msgs]
-      |> Kernel.to_string()
+      |> to_string()
 
     total_received_bytes =
       device_data[:total_received_bytes]
-      |> Kernel.to_string()
+      |> to_string()
 
     last_connection =
       device_data[:last_connection]
       |> DateTime.from_unix!(:millisecond)
-      |> DateTime.to_string()
+      |> DateTime.to_iso8601()
 
     last_disconnection =
       device_data[:last_disconnection]
       |> DateTime.from_unix!(:millisecond)
-      |> DateTime.to_string()
+      |> DateTime.to_iso8601()
 
     last_seen_ip =
       device_data[:last_seen_ip]
       |> :inet_parse.ntoa()
-      |> Kernel.to_string()
+      |> to_string()
 
-    interface_details = gen_interface_details(conn, device_data)
+    interface_details = gen_interface_details(conn, realm, device_data)
 
     %State{
       device_id: device_id,
@@ -121,27 +114,28 @@ defmodule Astarte.Export.FetchData do
     }
   end
 
-  defp gen_interface_details(conn, device_data) do
+  defp gen_interface_details(conn, realm, device_data) do
     device_id = device_data[:device_id]
     introspection = device_data[:introspection]
     introspection_minor = device_data[:interospection_minor]
 
     Enum.reduce(introspection, [], fn {interface_name, major_version}, acc ->
       {:ok, interface_description} =
-        Interface.fetch_interface_descriptor(conn, interface_name, major_version)
+        Queries.fetch_interface_descriptor(conn, realm, interface_name, major_version)
 
-      minor_version = interface_description.minor_version |> Kernel.to_string()
+      minor_version = interface_description.minor_version |> to_string()
       major_version1 = major_version |> Kernel.to_string()
       interface_id = interface_description.interface_id
       aggregation = interface_description.aggregation
       storage = interface_description.storage
       interface_type = interface_description.type
-      {:ok, mappings} = Mappings.fetch_interface_mappings(conn, interface_id)
+      {:ok, mappings} = Queries.fetch_interface_mappings(conn, realm, interface_id)
 
       mapped_data_fields =
         Enum.reduce(mappings, [], fn mapping, acc1 ->
           endpoint_id = mapping.endpoint_id
           path = mapping.endpoint
+          data_type = mapping.value_type
 
           values =
             case interface_type do
@@ -150,19 +144,33 @@ defmodule Astarte.Export.FetchData do
                   :individual ->
                     fetch_individual_datastream_values(
                       conn,
-                      storage,
+                      realm,
                       device_id,
                       interface_id,
                       endpoint_id,
-                      path
+                      path,
+                      data_type
                     )
 
                   :object ->
-                    fetch_object_datastream_value(conn, storage, device_id, path)
+                    fetch_object_datastream_value(
+                      conn,
+                      realm,
+                      storage,
+                      device_id,
+                      path,
+                      data_type
+                    )
                 end
 
               :properties ->
-                fetch_individual_properties_values(conn, storage, device_id, interface_id)
+                fetch_individual_properties_values(
+                  conn,
+                  realm,
+                  device_id,
+                  interface_id,
+                  data_type
+                )
             end
 
           case values do
@@ -189,118 +197,104 @@ defmodule Astarte.Export.FetchData do
 
   defp fetch_individual_datastream_values(
          conn,
-         storage,
+         realm,
          device_id,
          interface_id,
          endpoint_id,
-         interface_name
+         path,
+         data_type
        ) do
-    statement = """
-     SELECT double_value, reception_timestamp FROM  #{storage} WHERE device_id=:deviceid AND
-     interface_id=:interfaceid AND endpoint_id=:endpointid AND
-     path=:interfacename
-    """
+    data_field = get_data_field_name(data_type)
 
-    interface_query =
-      Query.new()
-      |> Query.statement(statement)
-      |> Query.put(:deviceid, device_id)
-      |> Query.put(:interfaceid, interface_id)
-      |> Query.put(:endpointid, endpoint_id)
-      |> Query.put(:interfacename, interface_name)
-
-    {:ok, result} = Query.call(conn, interface_query)
+    {:ok, result} =
+      Queries.retrive_individual_datastreams(
+        conn,
+        realm,
+        device_id,
+        interface_id,
+        endpoint_id,
+        path,
+        data_field
+      )
 
     values =
-      CQEx.Result.all_rows(result)
-      |> Enum.map(fn list ->
-        double_value =
-          list[:double_value]
+      Enum.to_list(result)
+      |> Enum.map(fn map ->
+        atom_data_field = String.to_atom(data_field)
+
+        return_value =
+          map[atom_data_field]
           |> to_charlist
 
         reception_timestamp =
-          list[:reception_timestamp]
+          map[:reception_timestamp]
           |> DateTime.from_unix!(:millisecond)
-          |> DateTime.to_string()
+          |> DateTime.to_iso8601()
 
-        List.keyreplace(list, :double_value, 0, {:double_value, double_value})
-        |> List.keyreplace(:reception_timestamp, 0, {:reception_timestamp, reception_timestamp})
+        Map.replace!(map, atom_data_field, return_value)
+        |> Map.replace!(:reception_timestamp, reception_timestamp)
       end)
-
-    # {:storage_type => {:datastream, :individual}, :values => values }
   end
 
-  defp fetch_object_datastream_value(conn, storage, device_id, interface_name) do
-    statement = """
-      SELECT reception_timestamp, v_realpathdatavalue  from #{storage} where device_id=:deviceid AND path=:interfacename
-    """
-
-    interface_query =
-      Query.new()
-      |> Query.statement(statement)
-      |> Query.put(:deviceid, device_id)
-      |> Query.put(:interfacename, interface_name)
-
-    {:ok, result} = Query.call(conn, interface_query)
+  defp fetch_object_datastream_value(conn, realm, storage, device_id, path, data_field) do
+    {:ok, result} =
+      Queries.retrive_object_datastream_value(conn, realm, storage, device_id, path, data_field)
 
     values =
       Result.all_rows(result)
-      |> Enum.map(fn list ->
+      |> Enum.map(fn map ->
         reception_timestamp =
-          list[:reception_timestamp]
+          map[:reception_timestamp]
           |> DateTime.from_unix!(:millisecond)
-          |> DateTime.to_string()
+          |> DateTime.to_iso8601()
 
         v_realpathdatavalue =
-          list[:v_realpathdatavalue]
+          map[:v_realpathdatavalue]
           |> Kernel.to_string()
 
-        List.keyreplace(
-          list,
-          :reception_timestamp,
-          0,
-          {:reception_timestamp, reception_timestamp}
-        )
-        |> List.keyreplace(:v_realpathdatavalue, 0, {:v_realpathdatavalue, v_realpathdatavalue})
+        Map.replace!(map, :reception_timestamp, reception_timestamp)
+        |> Map.replace!(:v_realpathdatavalue, v_realpathdatavalue)
       end)
-
-    # {:storage_type => {:datastream, :object}, :values => values}
   end
 
-  defp fetch_individual_properties_values(conn, storage, device_id, interface_id) do
-    statement = """
-     SELECT  double_value, reception_timestamp , path from #{storage} where device_id=:deviceid AND interface_id=:interfaceid
-    """
-
-    interface_query =
-      Query.new()
-      |> Query.statement(statement)
-      |> Query.put(:deviceid, device_id)
-      |> Query.put(:interfaceid, interface_id)
-
-    {:ok, result} = Query.call(conn, interface_query)
+  defp fetch_individual_properties_values(conn, realm, device_id, interface_id, data_field) do
+    result =
+      Queries.retrive_individual_properties(conn, realm, device_id, interface_id, data_field)
 
     values =
       Result.all_rows(result)
-      |> Enum.map(fn list ->
+      |> Enum.map(fn map ->
         reception_timestamp =
-          list[:reception_timestamp]
+          map[:reception_timestamp]
           |> DateTime.from_unix!(:millisecond)
-          |> DateTime.to_string()
+          |> DateTime.to_iso8601()
 
         v_realpathdatavalue =
-          list[:v_realpathdatavalue]
+          map[:v_realpathdatavalue]
           |> Kernel.to_string()
 
-        List.keyreplace(
-          list,
-          :reception_timestamp,
-          0,
-          {:reception_timestamp, reception_timestamp}
-        )
-        |> List.keyreplace(:v_realpathdatavalue, 0, {:v_realpathdatavalue, v_realpathdatavalue})
+        Map.replace!(map, :reception_timestamp, reception_timestamp)
+        |> Map.replace!(:v_realpathdatavalue, v_realpathdatavalue)
       end)
+  end
 
-    # {:storage_type => {properties: :individual}, :values => values}
+  def get_data_field_name(data_type) when is_atom(data_type) do
+    case data_type do
+      :double -> "double_value"
+      :integer -> "integer_value"
+      :boolean -> "boolean_value"
+      :longinteger -> "longinteger_value"
+      :string -> "string_value"
+      :binaryblob -> "binaryblob_value"
+      :datetime -> "datetime_value"
+      :doublearray -> "doublearray_value"
+      :integerarray -> "integerarray_value"
+      :booleanarray -> "booleanarray_value"
+      :longintegerarray -> "longintegerarray_value"
+      :stringarray -> "stringarray_value"
+      :binaryblobarray -> "binaryblobarray_value"
+      :datetimearray -> "datetimearray_value"
+      _ -> :error
+    end
   end
 end
--- tools/astarte_export/lib/fetchdata/queries/queries.ex
+++ tools/astarte_export/lib/fetchdata/queries/queries.ex
@@ -1,7 +1,7 @@
 defmodule Astarte.Export.FetchData.Queries do
   alias Xandra
   alias Astarte.Import.LogFmtFormatter
-  alias Astarte.Core.InterfaceDescriptor 
+  alias Astarte.Core.InterfaceDescriptor
   alias Astarte.Core.Mapping
   require Logger
 
@@ -17,7 +17,6 @@ defmodule Astarte.Export.FetchData.Queries do
   end
 
   def retrive_interface_row(conn, realm, interface, major_version) do
-
     interface_statement = """
     SELECT name, major_version, minor_version, interface_id, type, ownership, aggregation,
       storage, storage_type, automaton_transitions, automaton_accepting_states
@@ -25,13 +24,13 @@ defmodule Astarte.Export.FetchData.Queries do
     WHERE name=? AND major_version=?
     """
 
-    params = [{"ascii", interface}, 
-              {"int", major_version}
-             ]
- 
-    options = [uuid_format: :binary, 
-               consistency: :quorum
-              ]
+    params = [{"ascii", interface}, {"int", major_version}]
+
+    options = [
+      consistency: :quorum,
+      uuid_format: :binary,
+      timestamp_format: :integer
+    ]
 
     with {:ok, result} <- Xandra.execute(conn, interface_statement, params, options) do
       {:ok, result}
@@ -49,29 +48,28 @@ defmodule Astarte.Export.FetchData.Queries do
   def fetch_interface_descriptor(conn, realm, interface, major_version) do
     with {:ok, interface_row} <- retrive_interface_row(conn, realm, interface, major_version) do
       interface_row
-      |> Enum.to_list
+      |> Enum.to_list()
       |> hd
-      |> InterfaceDescriptor.from_db_result      
+      |> InterfaceDescriptor.from_db_result()
     end
   end
 
   def stream_devices(conn, realm, options) do
     devices_statement = """
-      SELECT * from #{realm}.devices 
-      """
+    SELECT * from #{realm}.devices 
+    """
+
     params = []
 
     query_options =
-      [date_format: :integer, 
-       uuid_format: :binary] 
-      ++
-      with value when is_integer(value) <- Keyword.get(options, :page_size) do
-        with nil <- Keyword.get(options, :paging_state) do
-          [page_size: value]
-        else
-          paging_value -> [page_size: 1, paging_state: paging_value]
+      [timestamp_format: :integer, uuid_format: :binary] ++
+        with value when is_integer(value) <- Keyword.get(options, :page_size) do
+          with nil <- Keyword.get(options, :paging_state) do
+            [page_size: value]
+          else
+            paging_value -> [page_size: 1, paging_state: paging_value]
+          end
         end
-      end
 
     with {:ok, result} = Xandra.execute(conn, devices_statement, params, query_options) do
       {:ok, result}
@@ -93,16 +91,17 @@ defmodule Astarte.Export.FetchData.Queries do
     FROM #{realm}.endpoints
     WHERE interface_id=?
     """
+
     params = [{"uuid", interface_id}]
 
-    options = [uuid_format: :binary]
+    options = [uuid_format: :binary, timestamp_format: :integer]
 
     with {:ok, result} = Xandra.execute(conn, mappings_statement, params, options) do
-      mappings = 
-      result
-      |> Enum.to_list
-      
-      mappings_1 = Enum.map(mappings, &Mapping.from_db_result!/1) 
+      mappings =
+        result
+        |> Enum.to_list()
+
+      mappings_1 = Enum.map(mappings, &Mapping.from_db_result!/1)
       {:ok, mappings_1}
     else
       {:error, %Xandra.Error{message: message} = err} ->
@@ -113,22 +112,20 @@ defmodule Astarte.Export.FetchData.Queries do
         # Logger.error("database connection error.", log_metadata(realm, device_id, err))
         {:error, :database_error}
     end
-  end  
+  end
 
- def retrive_individual_properties(conn, realm, device_id, interface_id, data_type) do
-   properties_statement = """
-   SELECT  #{data_type}, reception_timestamp , path from #{realm}.individual_properties 
-     where device_id=? AND interface_id=?
-   """
-   
-   params = [{"uuid", device_id},
-             {"uuid", interface_id}] 
+  def retrive_individual_properties(conn, realm, device_id, interface_id, data_type) do
+    properties_statement = """
+    SELECT  #{data_type}, reception_timestamp , path from #{realm}.individual_properties 
+      where device_id=? AND interface_id=?
+    """
 
-   options = [uuid_format: :binary,
-              date_format: :integer] 
-  
+    params = [{"uuid", device_id}, {"uuid", interface_id}]
+
+    options = [uuid_format: :binary, timestamp_format: :integer]
+
     with {:ok, result} = Xandra.execute(conn, properties_statement, params, options) do
-      result |> Enum.to_list
+      {:ok, result}
     else
       {:error, %Xandra.Error{message: message} = err} ->
         # Logger.error("database error: #{message}.", log_metadata(realm, device_id, err))
@@ -140,46 +137,47 @@ defmodule Astarte.Export.FetchData.Queries do
     end
   end
 
-  def retrive_individual_datastreams(conn, realm, device_id, interface_id, endpoint_id, path, data_type) do
+  def retrive_individual_datastreams(
+        conn,
+        realm,
+        device_id,
+        interface_id,
+        endpoint_id,
+        path,
+        data_type
+      ) do
     individual_datastream_statement = """
     SELECT #{data_type}, reception_timestamp FROM  #{realm}.individual_datastreams WHERE device_id=? AND
       interface_id=? AND endpoint_id=? AND path=?
     """
-    
-    params = [{"uuid", device_id},
-              {"uuid", interface_id},
-              {"uuid", endpoint_id},
-              {"text", path}]
-    
-    options = [uuid_format: :binary,
-               date_format: :integer] 
+
+    params = [{"uuid", device_id}, {"uuid", interface_id}, {"uuid", endpoint_id}, {"text", path}]
+    options = [uuid_format: :binary, timestamp_format: :integer]
 
     with {:ok, result} = Xandra.execute(conn, individual_datastream_statement, params, options) do
-      result |> Enum.to_list
+      {:ok, result}
     else
       {:error, %Xandra.Error{message: message} = err} ->
         # Logger.error("database error: #{message}.", log_metadata(realm, device_id, err))
         {:error, :database_error}
-    
+
       {:error, %Xandra.ConnectionError{} = err} ->
         # Logger.error("database connection error.", log_metadata(realm, device_id, err))
         {:error, :database_error}
     end
   end
-  
+
   defp retrive_object_datastream_value(conn, realm, storage, device_id, path, data_type) do
     object_datastream_statement = """
       SELECT reception_timestamp, #{data_type}  from #{realm}.#{storage} where device_id=? AND path=?
     """
-     
-    params = [{"uuid", device_id},
-              {"text", path}]
 
-    options = [uuid_format: :binary,
-               date_format: :integer]
+    params = [{"uuid", device_id}, {"text", path}]
+
+    options = [uuid_format: :binary, timestamp_format: :integer]
 
     with {:ok, result} = Xandra.execute(conn, object_datastream_statement, params, options) do
-      result |> Enum.to_list
+      {:ok, result}
     else
       {:error, %Xandra.Error{message: message} = err} ->
         # Logger.error("database error: #{message}.", log_metadata(realm, device_id, err))
@@ -190,4 +188,4 @@ defmodule Astarte.Export.FetchData.Queries do
         {:error, :database_error}
     end
   end
-end 
+end
