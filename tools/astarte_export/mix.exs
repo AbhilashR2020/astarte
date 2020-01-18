09a66c9b7f87b39e44740bad13656941b25b3d8b
Harika <abhilash.ramadugu52@gmail.com>
2020-01-18 13:11:16 +0530

    added changes to mix.exs

--- tools/astarte_export/mix.exs
+++ tools/astarte_export/mix.exs
@@ -21,12 +21,12 @@ defmodule AstarteExport.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
   [
-      {:xml_builder, git: "https://github.com/joshnuss/xml_builder.git" },
       {:xandra, "~> 0.10"},
       {:astarte_core, github: "astarte-platform/astarte_core"},
       {:astarte_data_access, github: "astarte-platform/astarte_data_access"},
       {:distillery, "~> 2.0.0"},
-      {:pretty_log, github: "ispirata/pretty_log"}
+      {:pretty_log, github: "ispirata/pretty_log"},
+      {:xml_stream_writer, github: "ispirata/xml_stream_writer"}
   ]
   end
 end
